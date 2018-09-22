43f65080a2897105fe06a177d3be3ab7706b9e1f
kdabrowska <krystyna.dabrowska@onet.pl>
2018-09-22 18:01:11 +0200

    dividing by zero doesn't work

--- calculator.py
+++ calculator.py
@@ -2,7 +2,23 @@ import tkinter as tk
 
 root = tk.Tk()
 colour = 'cyan'
-root.configure(background=colour, width=300)
+root.configure(background=colour, width=50)
+button_width = 7
+button_height = 3
+first_number = 0
+#second_number = 0
+operator = ""
+
+
+"""insert box & result box"""
+#pierwsza_liczba = tk.StringVar()
+#druga_liczba = tk.StringVar()
+result_box = tk.Text(root)
+result_box.grid(row=0, column=0)
+result_box.config(width=30, height=3)
+insert_box = tk.Text(root)
+insert_box.grid(row=1, column=0)
+insert_box.config(width=30, height=3)
 
 
 def convertstr(s):
@@ -16,87 +32,170 @@ def convertstr(s):
 
 def add():
     result_box.delete('1.0', tk.END)
-    a = first_number.get()
-    b = second_number.get()
-    if a and b:
-        result = convertstr(a)+convertstr(b)
-    else:
-        result = "insert both numbers"
-    result_box.insert(tk.INSERT, result)
-    first_number.set('')
-    second_number.set('')
+    a = insert_box.get('1.0', tk.END)
+    a = a[0:-1]
+    globals()["first_number"] = a
+    globals()["operator"] = "+"
+    insert = a+"+"
+    result_box.insert(tk.INSERT, insert)
+    insert_box.delete('1.0', tk.END)
 
 
 def substract():
     result_box.delete('1.0', tk.END)
-    a = first_number.get()
-    b = second_number.get()
-    if a and b:
-        result = convertstr(a)-convertstr(b)
-    else:
-        result = "insert both numbers"
-    result_box.insert(tk.INSERT, result)
-    first_number.set('')
-    second_number.set('')
+    a = insert_box.get('1.0', tk.END)
+    a = a[0:-1]
+    globals()["first_number"] = a
+    globals()["operator"] = "-"
+    insert = a + "-"
+    result_box.insert(tk.INSERT, insert)
+    insert_box.delete('1.0', tk.END)
 
 
 def multiply():
     result_box.delete('1.0', tk.END)
-    a = first_number.get()
-    b = second_number.get()
-    if a and b:
-        result = convertstr(a)*convertstr(b)
-    else:
-        result = "insert both numbers"
-    result_box.insert(tk.INSERT, result)
-    first_number.set('')
-    second_number.set('')
+    a = insert_box.get('1.0', tk.END)
+    a = a[0:-1]
+    globals()["first_number"] = a
+    globals()["operator"] = "*"
+    insert = a + "*"
+    result_box.insert(tk.INSERT, insert)
+    insert_box.delete('1.0', tk.END)
 
 
 def divide():
     result_box.delete('1.0', tk.END)
-    a = first_number.get()
-    b = second_number.get()
-    print(b)
-    if a and b:
-        if convertstr(b) == 0:
-            result = "you can not divide by zero!!"
-        else:
-            result = convertstr(a)/convertstr(b)
+    a = insert_box.get('1.0', tk.END)
+    a = a[0:-1]
+    globals()["first_number"] = a
+    globals()["operator"] = '/'
+    insert = a + "/"
+    result_box.insert(tk.INSERT, insert)
+    insert_box.delete('1.0', tk.END)
+
+
+def result_of_operation():
+    second_number = insert_box.get('1.0', tk.END)
+    #print(str(second_number))
+    #print(operator == '/')
+    #print(str(second_number) == str(0))
+    #print("type of second_number", type(second_number))
+    #print(second_number)
+    #print("type of opertor", type(operator))
+    #print(operator)
+    if operator == '+':
+        result = convertstr(first_number)+convertstr(second_number)
+    elif operator == '-':
+        result = convertstr(first_number) - convertstr(second_number)
+    elif operator == '*':
+        result = convertstr(first_number) * convertstr(second_number)
+    elif operator == '/' and second_number == "0":
+        result = "you can not divide by zero"
     else:
-        result = "insert both numbers"
+        print('tutaj')
+        result = convertstr(first_number)/convertstr(second_number)
+    result = first_number+operator+second_number[0:-1]+'='+str(result)
+    result_box.delete('1.0', tk.END)
+    insert_box.delete('1.0', tk.END)
     result_box.insert(tk.INSERT, result)
-    first_number.set('')
-    second_number.set('')
 
 
-"""labels """
-
-tk.Label(text='Insert first number').grid(row=0, column=0)
-tk.Label(text='Insert second number').grid(row=1, column=0)
-
-"""string variables"""
-
-first_number = tk.StringVar()
-second_number = tk.StringVar()
+def clear():
+    insert_box.delete('1.0', tk.END)
+    result_box.delete('1.0', tk.END)
 
 
-entry_first_number = tk.Entry(root, textvariable=first_number).grid(row=0, column = 1)
-entry_second_number = tk.Entry(root, textvariable=second_number).grid(row=1, column=1)
+def back():
+    insert_box.delete('end - 2 chars')
+    result_box.delete('end - 2 chars')
 
+
+def insert_zero():
+    insert_box.insert(tk.INSERT, 0)
+
+
+def insert_one():
+    insert_box.insert(tk.INSERT, 1)
+
+
+def insert_two():
+    insert_box.insert(tk.INSERT, 2)
+
+
+def insert_three():
+    insert_box.insert(tk.INSERT, 3)
+
+
+def insert_four():
+    insert_box.insert(tk.INSERT, 4)
+
+
+def insert_five():
+    insert_box.insert(tk.INSERT, 5)
+
+
+def insert_six():
+    insert_box.insert(tk.INSERT, 6)
+
+
+def insert_seven():
+    insert_box.insert(tk.INSERT, 7)
+
+
+def insert_eight():
+    insert_box.insert(tk.INSERT, 8)
+
+
+def insert_nine():
+    insert_box.insert(tk.INSERT, 9)
+
+
+def back():
+    result_box.delete('end - 2 chars')
+    insert_box.delete('end - 2 chars')
+
+def coma():
+    insert_box.insert(tk.INSERT, '.')
 
 """buttons"""
 child = tk.PanedWindow()
 child.grid(row=2, column=0, sticky=tk.N)
-tk.Button(child, text='+', command=add).grid(row=2, column=0)
-tk.Button(child, text='-', command=substract).grid(row=2, column=1)
-tk.Button(child, text='*', command=multiply ).grid(row=2, column=2)
-tk.Button(child, text='/',command=divide).grid(row=2, column=3)
-tk.Label(text='Result').grid(row=3, column=0)
+
+# first row of buttons
+tk.Button(child, text='CE', width=button_width, height=button_height).grid(row=2, column=0)
+tk.Button(child, text='C', command=clear, width=button_width, height=button_height).grid(row=2, column=1)
+tk.Button(child, text='Back', command=back, width=button_width, height=button_height).grid(row=2, column=2)
+tk.Button(child, text='/', width=button_width, height=button_height, command=divide).grid(row=2, column=3)
+
+# second row of buttons
+tk.Button(child, text='7', command=insert_seven, width=button_width, height=button_height).grid(row=3, column=0)
+tk.Button(child, text='8', command=insert_eight, width=button_width, height=button_height).grid(row=3, column=1)
+tk.Button(child, text='9', command=insert_nine, width=button_width, height=button_height).grid(row=3, column=2)
+tk.Button(child, text='*', width=button_width, height=button_height, command=multiply).grid(row=3, column=3)
+
+# third row of buttons
+tk.Button(child, text='4', command=insert_four, width=button_width, height=button_height).grid(row=4, column=0)
+tk.Button(child, text='5', command=insert_five, width=button_width, height=button_height).grid(row=4, column=1)
+tk.Button(child, text='6', command=insert_six, width=button_width, height=button_height).grid(row=4, column=2)
+tk.Button(child, text='-', width=button_width, height=button_height, command=substract).grid(row=4, column=3)
+
+# fourth row of buttons
+tk.Button(child, text='1', command=insert_one, width=button_width, height=button_height).grid(row=5, column=0)
+tk.Button(child, text='2', command=insert_two, width=button_width, height=button_height).grid(row=5, column=1)
+tk.Button(child, text='3', command=insert_three, width=button_width, height=button_height).grid(row=5, column=2)
+tk.Button(child, text='+', width=button_width, height=button_height, command=add).grid(row=5, column=3)
+
+# fifth row of buttons
+tk.Button(child, text='+-', width=button_width, height=button_height).grid(row=6, column=0)
+tk.Button(child, text='0', command=insert_zero, width=button_width, height=button_height).grid(row=6, column=1)
+tk.Button(child, text='.', width=button_width, height=button_height, command=coma).grid(row=6, column=2)
+tk.Button(child, text='=', width=button_width, height=button_height, command=result_of_operation).grid(row=6, column=3)
+
+# tk.Label(text='Result').grid(row=3, column=0)
 
 """result box"""
-result_box = tk.Text(root)
-result_box.grid(row=3, column=1)
-result_box.config(width=10, height=5)
+# result_box = tk.Text(root)
+# result_box.grid(row=3, column=1)
+# result_box.config(width=10, height=5)
 
 root.mainloop()
