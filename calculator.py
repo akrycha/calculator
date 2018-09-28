19c321223d1375272615a6786617ead193c8e73f
kdabrowska <krystyna.dabrowska@onet.pl>
2018-09-28 15:36:10 +0200

    works only on two numbers

--- calculator.py
+++ calculator.py
@@ -31,74 +31,73 @@ def convertstr(s):
 
 
 def add():
-    result_box.delete('1.0', tk.END)
-    a = insert_box.get('1.0', tk.END)
-    a = a[0:-1]
-    globals()["first_number"] = a
-    globals()["operator"] = "+"
-    insert = a+"+"
-    result_box.insert(tk.INSERT, insert)
-    insert_box.delete('1.0', tk.END)
+    b = result_box.get('1.0', tk.END)[0:-1]
+    a = insert_box.get('1.0', tk.END)[0:-1]
+    if a != '' and b == '':
+        result_box.delete('1.0', tk.END)
+        globals()["first_number"] = a
+        globals()["operator"] = "+"
+        insert = a+"+"
+        result_box.insert(tk.INSERT, insert)
+        insert_box.delete('1.0', tk.END)
 
 
 def substract():
-    result_box.delete('1.0', tk.END)
-    a = insert_box.get('1.0', tk.END)
-    a = a[0:-1]
-    globals()["first_number"] = a
-    globals()["operator"] = "-"
-    insert = a + "-"
-    result_box.insert(tk.INSERT, insert)
-    insert_box.delete('1.0', tk.END)
+    b = result_box.get('1.0', tk.END)[0:-1]
+    a = insert_box.get('1.0', tk.END)[0:-1]
+    if a != '' and b == '':
+        result_box.delete('1.0', tk.END)
+        globals()["first_number"] = a
+        globals()["operator"] = "-"
+        insert = a + "-"
+        result_box.insert(tk.INSERT, insert)
+        insert_box.delete('1.0', tk.END)
 
 
 def multiply():
-    result_box.delete('1.0', tk.END)
-    a = insert_box.get('1.0', tk.END)
-    a = a[0:-1]
-    globals()["first_number"] = a
-    globals()["operator"] = "*"
-    insert = a + "*"
-    result_box.insert(tk.INSERT, insert)
-    insert_box.delete('1.0', tk.END)
+    b = result_box.get('1.0', tk.END)[0:-1]
+    a = insert_box.get('1.0', tk.END)[0:-1]
+    if a != '' and b == '':
+        result_box.delete('1.0', tk.END)
+        globals()["first_number"] = a
+        globals()["operator"] = "*"
+        insert = a + "*"
+        result_box.insert(tk.INSERT, insert)
+        insert_box.delete('1.0', tk.END)
 
 
 def divide():
-    result_box.delete('1.0', tk.END)
-    a = insert_box.get('1.0', tk.END)
-    a = a[0:-1]
-    globals()["first_number"] = a
-    globals()["operator"] = '/'
-    insert = a + "/"
-    result_box.insert(tk.INSERT, insert)
-    insert_box.delete('1.0', tk.END)
+    b = result_box.get('1.0', tk.END)[0:-1]
+    a = insert_box.get('1.0', tk.END)[0:-1]
+    if a != '' and b == '':
+        result_box.delete('1.0', tk.END)
+        globals()["first_number"] = a
+        globals()["operator"] = '/'
+        insert = a + "/"
+        result_box.insert(tk.INSERT, insert)
+        insert_box.delete('1.0', tk.END)
 
 
 def result_of_operation():
-    second_number = insert_box.get('1.0', tk.END)
-    second_number = second_number[0:-1]
-    #print(str(second_number))
-    #print(operator == '/')
-    #print(str(second_number) == str(0))
-    #print("type of second_number", type(second_number))
-    #print(second_number)
-    #print("type of opertor", type(operator))
-    #print(operator)
-    if operator == '+':
-        result = convertstr(first_number)+convertstr(second_number)
-    elif operator == '-':
-        result = convertstr(first_number) - convertstr(second_number)
-    elif operator == '*':
-        result = convertstr(first_number) * convertstr(second_number)
-    elif operator == '/' and second_number == "0":
-        result = "you can not divide by zero"
-    else:
-        print('tutaj')
-        result = convertstr(first_number)/convertstr(second_number)
-    result = first_number+operator+second_number+'='+str(result)
-    result_box.delete('1.0', tk.END)
-    insert_box.delete('1.0', tk.END)
-    result_box.insert(tk.INSERT, result)
+    b = result_box.get('1.0', tk.END)[0:-1]
+    a = insert_box.get('1.0', tk.END)[0:-1]
+    if a != '' and b != '':
+        second_number = insert_box.get('1.0', tk.END)
+        second_number = second_number[0:-1]
+        if operator == '+':
+            result = convertstr(first_number)+convertstr(second_number)
+        elif operator == '-':
+            result = convertstr(first_number) - convertstr(second_number)
+        elif operator == '*':
+            result = convertstr(first_number) * convertstr(second_number)
+        elif operator == '/' and second_number == "0":
+            result = "you can not divide by zero"
+        else:
+            result = convertstr(first_number)/convertstr(second_number)
+        result = first_number+operator+second_number+'='+str(result)
+        result_box.delete('1.0', tk.END)
+        insert_box.delete('1.0', tk.END)
+        result_box.insert(tk.INSERT, result)
 
 
 def clear():
@@ -157,8 +156,27 @@ def back():
 
 
 def coma():
-    if '.' not in insert_box.get('1.0', tk.END):
+    a = insert_box.get('1.0', tk.END)
+    a = a[0:-1]
+    print('a=', a)
+    if a == '':
+        insert_box.insert(tk.INSERT, '0.')
+    elif '.' not in insert_box.get('1.0', tk.END):
         insert_box.insert(tk.INSERT, '.')
+
+
+def pm():
+    a = insert_box.get('1.0', tk.END)
+    a = a[0:-1]
+    if a != '':
+        a = convertstr(insert_box.get('1.0', tk.END))
+        print(a)
+        if a > 0:
+            a = '-'+str(a)
+        else:
+            a = str(a[1:])
+        insert_box.delete('1.0', tk.END)
+        insert_box.insert(tk.INSERT, a)
 
 
 """buttons"""
@@ -190,16 +208,10 @@ tk.Button(child, text='3', command=insert_three, width=button_width, height=butt
 tk.Button(child, text='+', width=button_width, height=button_height, command=add).grid(row=5, column=3)
 
 # fifth row of buttons
-tk.Button(child, text='+-', width=button_width, height=button_height).grid(row=6, column=0)
+tk.Button(child, text='+-', width=button_width, height=button_height, command=pm).grid(row=6, column=0)
 tk.Button(child, text='0', command=insert_zero, width=button_width, height=button_height).grid(row=6, column=1)
 tk.Button(child, text='.', width=button_width, height=button_height, command=coma).grid(row=6, column=2)
 tk.Button(child, text='=', width=button_width, height=button_height, command=result_of_operation).grid(row=6, column=3)
 
-# tk.Label(text='Result').grid(row=3, column=0)
-
-"""result box"""
-# result_box = tk.Text(root)
-# result_box.grid(row=3, column=1)
-# result_box.config(width=10, height=5)
 
 root.mainloop()
